6edd1e1165e60c8a8994c5700db75652e0100049
Damini Jaiswal <damini.jaiswal@techinfini.com>
2018-04-06 13:14:39 +0530

    new version

---
+++ flexmls-idx/lib/account.php
@@ -0,0 +1,28 @@
+<?php
+
+class FMC_Account {
+
+	function __construct($data) {
+		if( count( $data ) && ( is_array( $data ) ) ){
+			foreach ($data as $property => $value) {
+				$this->$property = $value;
+			}
+		}
+	}
+
+	function primary_email() {
+		if( count( $this->Emails ) ){
+			foreach ($this->Emails as $email) {
+				if(array_key_exists("Primary", $email)) {
+					return $email["Address"];
+				}
+			}
+			if(sizeof($this->Emails) > 0) {
+				return $this->Emails[0]["Address"];
+			}
+		}
+		return false;
+	}
+
+
+}
